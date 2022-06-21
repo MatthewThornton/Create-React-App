f8e0a35bac327ad133be3d29468a374e0b3f9b6b
Matthew Thornton <matthewthornton.usa@gmail.com>
2022-06-21 11:32:17 -0700

    Updates

--- src/components/Header/Profile.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, } from '@testing-library/react';
-import Profile from "./Profile";
-
-
-test('should render user info', async () => {
-    render(<Profile />)
-    const user = await screen.findByTestId('USER-NAME');
-    expect(user).toBeVisible();
-});
